e720d023f7f969c881363928b3281fc9c93abe9e
BlackChao-Script <83378274+BlackChao-Script@users.noreply.github.com>
2022-04-02 09:34:40 +0800

    初始化_table

---
+++ pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="index">
+		<text>{{ title }}</text>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: '主页'
+		}
+	},
+	onLoad() {},
+	methods: {}
+}
+</script>
+
+<style></style>
---
+++ pages/sort/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="sort">
+		<text>{{ title }}</text>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			title: '分类'
+		}
+	}
+}
+</script>
+
+<style></style>
